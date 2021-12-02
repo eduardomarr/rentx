37d67fc7e0737aa1fa84de07d1dacff513429aa9
Eduardo Marques Nepomuceno de Sousa <dado.nep@gmail.com>
2021-12-02 15:28:10 -0300

    fonts bug

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-// import { useFonts } from 'expo-font';
 
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
@@ -19,11 +18,6 @@ export default function App() {
     Archivo_400Regular,
     Archivo_500Medium,
     Archivo_600SemiBold,
-    // Inter_400Regular: require('./src/assets/fonts/Inter-Regular.ttf'),
-    // Inter_500Medium: require('./src/assets/fonts/Inter-Medium.ttf'),
-    // Archivo_400Regular: require('./src/assets/fonts/Archivo-Regular.ttf'),
-    // Archivo_500Medium: require('./src/assets/fonts/Archivo-Medium.ttf'),
-    // Archivo_600SemiBold: require('./src/assets/fonts/Archivo-SemiBold.ttf'),
   });
 
   if (!fontsLoaded) {
